9de9c4109293af9a9bdb30b7043ba6b9fb08dba8
Vignesh-PyDev <vigneshwarann@sase.ssn.edu.in>
2022-09-15 11:49:29 +0530

    Added Brownie Points Code modified in Views.py and added Script file for Weather Data refresh

--- Infilect_Assesment/Infilect_Weather_API_Assignment/Infilect/scripts.py
+++ Infilect_Assesment/Infilect_Weather_API_Assignment/Infilect/scripts.py
@@ -21,24 +21,12 @@ def Brownie_Point_1():
     try:
 
         
-
-
         for x in City_Names.objects.all().values():
 
             
-
-            # Here according to Assignment i have to use https://openweathermap.org/api but its asks card details for api usage even for free plan
-
-            # so i found an alternate which is free till 27-SEP-2022.
-
-            # print("http://api.weatherapi.com/v1/current.json?key=8f88c80af0744fff9b631859221509&q='+x['city_name']+'&aqi=no")
-
             Loop_var = requests.get(r'http://api.weatherapi.com/v1/current.json?key=8f88c80af0744fff9b631859221509&q='+x['city_name']+'&aqi=no').json()
 
-            # print(Loop_var,"skjfhsdkjfh")
-
             
-
             Weather_Data = City_Weather_Data(
 
                 city_name = x['city_name'],
@@ -50,36 +38,12 @@ def Brownie_Point_1():
 
 
             Weather_Data.save()
-
-            
-
-            # Max_No += 1
-
-            # here 35 used to restrict loop
-
-            # if Max_No >35:break
-
-            # Here why I used 35 every time i call that api the new data in inserted 
-
-            # i checked with inspectdb its around 100+ rows inserted so i used 35 as given in Requirement.
-
-            # values() method used to convert queryset into json
-
-            
-
             
     except:
 
         pass
 
-    return 0
-
-
 Brownie_Point_1()
-
-# print(City_Weather_Data.objects.all())       
-
-# # print("Geeksforgeeks")
 
 schedule.every(10).minutes.do(Brownie_Point_1)
 
